5b456cbf2b326e70a8da0c957ff00138d6f7900d
Hamza Omer <Hamza.Omer@mail.bcu.ac.uk>
2018-11-07 10:34:15 +0000

    Lab Task

---
+++ src/Calculator.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Calculator {
+	
+	public ArrayList<Integer> inputValue = new ArrayList<Integer>();
+	
+	
+	public void loadConstant(int i) {
+		 inputValue.add(i);
+	}
+	
+	@Override
+	public String toString() {
+		return "[].";
+
+	}
+
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void total() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
